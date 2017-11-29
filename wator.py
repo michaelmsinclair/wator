ec75afe2e00569346b359c7564652cc3ae4bc3f2
Michael M. Sinclair <michaelmsinclair@gmail.com>
2017-11-29 16:41:05 -0500

    Removed display option, as the display is not the long pole to a turn, even with storing files. TBD - rework display, and other switches. Added ability to choose between the traditional movement, and enhanced.

--- wator.py
+++ wator.py
@@ -39,28 +39,38 @@ class SeaPosition(object):
     def getSeaPosition(self):
         return(self.x, self.y)
     
-    def getAdjacent(self):
+    def getAdjacent(self, traditional):
         """
-        Returns an array of tuples for the positions adjacent to the current position. 
-        Includes diagonal positions, that is the eight surrounding Pos. below:
+        returns two arrays of tuples for the positions adjacent to the position. 
+        the first contains location tuples for empty cells, while the second 
+        contains cells with a creature in it.
+        the cells can either be those in the n, e, s, w postion, or all eight
+        surrounding cells - n, ne, e, se, s, sw, w, ne.
+        traditional:
+                    (0,+1)
+            (-1, 0)  Pos.  (+1, 0)
+                    (0,-1) 
+        new:
             (-1,+1) (0,+1) (+1,+1)
             (-1, 0)  Pos.  (+1, 0)
             (-1,-1) (0,-1) (+1,-1)
         """
         empty = []
         occupied = []
-        for deltaX in (-1, 0, 1):
-            for deltaY in (-1, 0, 1):
-                if not (deltaX == 0 and deltaY == 0):
-                    newX = (self.x + deltaX + self.sea.getMaxX()) % self.sea.getMaxX()
-                    newY = (self.y + deltaY + self.sea.getMaxY()) % self.sea.getMaxY()
-                    if self.sea.isCellEmpty(newX,newY):
-                        empty.append((newX, newY))
-                    else:
-                        occupied.append((newX, newY))
+        search = []
+        if traditional:
+            search = [(0,+1),(-1, 0),(+1, 0),(0,-1)]
+        else:
+            search = [(-1,+1),(0,+1),(+1,+1),(-1, 0),(+1, 0),(-1,-1),(0,-1),(+1,-1)]
+        for s in search:
+            deltaX, deltaY = s[0], s[1]
+            newX = (self.x + deltaX + self.sea.getMaxX()) % self.sea.getMaxX()
+            newY = (self.y + deltaY + self.sea.getMaxY()) % self.sea.getMaxY()
+            if self.sea.isCellEmpty(newX,newY):
+                empty.append((newX, newY))
+            else:
+                occupied.append((newX, newY))
         return(empty,occupied)
-    
-    
     
     def __str__(self):
         return "(%d, %d)" % (self.x, self.y)
@@ -139,13 +149,13 @@ class Sea(object):
     def getFishes(self):
         return self.fishes
         
-    def addCreature(self, x, y, newCreature):
+    def addCreature(self, x, y, newCreature, t):
         '''
         If creature can be added to sea, add it to the list of creatures
         '''
         if self.isCellEmpty(x, y):
             pos = SeaPosition(x,y,self)
-            creature = newCreature(self, pos)
+            creature = newCreature(self, pos, t)
             self.setCell(x,y, creature)
             self.creatures[self.creatureTag] = creature
             self.creatureTag += 1
@@ -199,12 +209,13 @@ class Sea(object):
                     
 
 class Creature(object):
-    def __init__(self, sea, pos, spawnAge=2):
+    def __init__(self, sea, pos, traditional=True, spawnAge=2):
         '''
         Simple creature, reproduces quickly, does not eat, and never dies except if eaten.
         '''
         self.sea = sea
         self.pos = pos
+        self.traditional = traditional
         self.age = 0
         self.spawnAge = spawnAge
         self.alive = True
@@ -229,7 +240,7 @@ class Creature(object):
     def spawn(self,free):
         if self.age >= self.spawnAge:
             spawnX, spawnY = random.choice(free)
-            self.sea.addCreature(spawnX, spawnY, type(self))
+            self.sea.addCreature(spawnX, spawnY, type(self), self.traditional)
             self.age = 0
             return True
         else:
@@ -249,7 +260,7 @@ class Creature(object):
     def turn(self):
         if self.alive:
             self.age += 1
-            empty, occupied = self.pos.getAdjacent()
+            empty, occupied = self.pos.getAdjacent(self.traditional)
             if len(empty) > 0:
                 if not self.spawn(empty):
                     self.move(empty)
@@ -258,8 +269,8 @@ class Creature(object):
         return "%s" % str(self.pos)
     
 class Shark(Creature):
-    def __init__(self, sea, pos, spawnAge=5, starveAge=3):
-        Creature.__init__(self, sea, pos, spawnAge)
+    def __init__(self, sea, pos, traditional=True, spawnAge=5, starveAge=3):
+        Creature.__init__(self, sea, pos, traditional, spawnAge)
         self.starveAge = starveAge
         self.starve = 0
         
@@ -291,7 +302,7 @@ class Shark(Creature):
                 self.died()
             else:
                 spawnX, spawnY = self.pos.getSeaPosition()
-                empty, occupied = self.pos.getAdjacent()
+                empty, occupied = self.pos.getAdjacent(self.traditional)
                 if len(occupied) > 0:
                     if self.eat(occupied):
                         self.spawn([(spawnX, spawnY)])
@@ -309,11 +320,12 @@ class Fish(Creature):
     def __str__(self):
         return "%s %s Alive: %s  Age: %d" % ('Fish', Creature.__str__(self), str(self.alive), self.age)
     
-def wator(x,y,s,f,d='y'):
+def wator(x,y,s,f,traditional=False):
     '''
     x =  width of sea, y the height
     s = number of sharks, f the number of fishes
     d = y, display screen. if anything else, then give text output only.
+    t = traditional
     '''
     
     if (s+f) > x*y:
@@ -327,7 +339,7 @@ def wator(x,y,s,f,d='y'):
         while noCell:
             xS = random.randint(0,x-1)
             yS = random.randint(0,y-1)
-            if aSea.addCreature(xS,yS, Shark):
+            if aSea.addCreature(xS,yS,Shark,traditional):
                 noCell = False
 
     for fish in range(f):
@@ -335,18 +347,13 @@ def wator(x,y,s,f,d='y'):
         while noCell:
             xF = random.randint(0,x-1)
             yF = random.randint(0,y-1)
-            if aSea.addCreature(xF,yF, Fish):
+            if aSea.addCreature(xF,yF,Fish,traditional):
                 noCell = False
 
-    if d == 'y':
-        display = True
-    else:
-        display = False
     tick = 0
     while aSea.getSharks() != 0 and aSea.getFishes() != 0:  # in range(200):
         before = time.clock()
-        if display:
-            aSea.display()
+        aSea.display()
         elapsedDisp = time.clock() - before
         tick += 1
         theCreatures = aSea.creatures.copy()
@@ -365,12 +372,16 @@ parser.add_argument("-f", "--fishes", type=int,
 parser.add_argument("-s", "--sharks", type=int,
                     help="initial number of sharks",
                     default=0)
+parser.add_argument("-t", "--traditional", action="store_true",
+                    help="traditional search pattern",
+                    default=False)
 parser.add_argument("-x", type=int,
                     help="number of horizontal cells",
                     default=200)
 parser.add_argument("-y", type=int,
                     help="number of vertical cells",
                     default=200)
+
 args = parser.parse_args()
 total_cells = args.x * args.y
 if args.sharks == 0:
@@ -383,4 +394,4 @@ if args.fishes == 0:
 else:
     fishes = args.fishes 
     
-wator(args.x, args.y, sharks, fishes)
+wator(args.x, args.y, sharks, fishes, traditional=args.traditional)
